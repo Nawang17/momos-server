5dc3005755ce8a537619a2bf8e55c5f72f98e3e2
Nawang17 <nawangofficial1010@gmail.com>
2022-10-31 17:54:07 -0400

    added post delete route

--- routes/DELETE/deletepost.js
+++ routes/DELETE/deletepost.js
@@ -1,6 +1,7 @@
 "use strict";
 const router = require("express").Router();
 const { posts } = require("../../models");
+const { cloudinary } = require("../../utils/cloudinary");
 
 router.delete("/:postId", async (req, res) => {
   const { postId } = req.params;
@@ -19,6 +20,17 @@ router.delete("/:postId", async (req, res) => {
         if (findPost.postUser !== req.user.id) {
           res.status(400).send("You are not authorized to delete this post");
         } else {
+          if (findPost.imagekey) {
+            await cloudinary.uploader.destroy(
+              findPost.imagekey,
+              (err, result) => {
+                if (err) {
+                  res.status(500).send(err);
+                }
+                console.log("Image deleted from cloudinary");
+              }
+            );
+          }
           await posts.destroy({
             where: {
               id: postId,
@@ -26,6 +38,7 @@ router.delete("/:postId", async (req, res) => {
             },
           });
           res.status(200).send("Post deleted successfully");
+          console.log("post deleted successfully");
         }
       }
     } catch (error) {
