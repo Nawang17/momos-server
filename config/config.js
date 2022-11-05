9238cb0c81f5721bbe7a2231787b67a4b8b2a3c5
Nawang17 <nawangofficial1010@gmail.com>
2022-11-05 18:54:59 -0400

    port chnaged

--- config/config.js
+++ config/config.js
@@ -25,7 +25,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+
     dialect: "mysql",
     dialectOptions: {
       charset: "utf8mb4",
