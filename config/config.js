62b6d534c8184e8759782f9414cd6a16fd752b7b
Nawang17 <nawangofficial1010@gmail.com>
2022-11-05 18:43:44 -0400

    added port variable

--- config/config.js
+++ config/config.js
@@ -25,6 +25,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectOptions: {
       charset: "utf8mb4",
