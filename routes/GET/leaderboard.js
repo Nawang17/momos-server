b67917d43f2ef40ab2558b43be0afb381a41ed3b
Nawang17 <nawangofficial1010@gmail.com>
2022-12-14 22:53:39 -0500

    add more limit to leaderboard results

--- routes/GET/leaderboard.js
+++ routes/GET/leaderboard.js
@@ -5,6 +5,7 @@ const sequelize = require("sequelize");
 router.get("/", async (req, res) => {
   try {
     const getUsers = await users.findAll({
+      limit: 10,
       attributes: {
         exclude: [
           "password",
@@ -37,6 +38,54 @@ router.get("/", async (req, res) => {
                 posts.postUser = users.id
                 AND likes.userId != users.id
             )`),
+            "totalLikes",
+          ],
+        ],
+      },
+
+      order: [[sequelize.literal("totalposts + totalLikes"), "DESC"]],
+    });
+    return res.json({ leaderboard: getUsers });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Something went wrong");
+  }
+});
+router.get("/getall", async (req, res) => {
+  try {
+    const getUsers = await users.findAll({
+      attributes: {
+        exclude: [
+          "password",
+          "email",
+          "createdAt",
+          "updatedAt",
+          "imagekey",
+          "status",
+          "userid",
+          "",
+        ],
+        include: [
+          [
+            sequelize.literal(`(
+                      SELECT COUNT(*)
+                      FROM posts AS posts
+                      WHERE
+                          posts.postUser = users.id
+      
+                  )`),
+            "totalposts",
+          ],
+
+          [
+            sequelize.literal(`(
+                SELECT COUNT(*)
+                FROM posts AS posts
+                INNER JOIN likes AS likes ON likes.postId = posts.id
+                WHERE 
+                  posts.postUser = users.id
+                  AND likes.userId != users.id
+              )`),
             "totalLikes",
           ],
         ],
