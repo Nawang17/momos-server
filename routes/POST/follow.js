92328d2131bf8c5a57b8cf150dd242e881830a8e
“Nawang17” <nawangofficial1010@gmail.com>
2023-01-19 19:31:42 -0500

    made changes to follow user route by reducing queries and also using async lock to prevent duplicate request

--- routes/POST/follow.js
+++ routes/POST/follow.js
@@ -1,80 +1,111 @@
 "use strict";
 const router = require("express").Router();
 const { follows, notis, users } = require("../../models");
+const asyncLock = require("async-lock");
+const lock = new asyncLock();
 
+//route handler for follow request
 router.post("/", async (req, res) => {
-  const { followingid } = req.body;
-  if (!followingid) {
-    return res.status(400).send("followingid is required");
-  } else if (followingid === req.user.id) {
-    return res.status(400).send("You cannot follow yourself");
-  } else {
-    try {
-      const finduser = await users.findOne({
-        where: {
-          id: followingid,
-        },
-      });
-      if (!finduser) {
-        return res.status(400).send("You cannot follow a non-existent user");
-      } else if (finduser.status === "inactive") {
-        return res.status(400).send("User is inactive");
-      }
-
-      const findFollow = await follows.findOne({
-        where: {
-          followingid,
-          userid: req.user.id,
-        },
-      });
-
-      if (findFollow) {
-        await follows.destroy({
-          where: {
-            followingid,
-            userid: req.user.id,
-          },
-        });
-
-        return res.status(200).send({ followed: false });
-      } else {
-        const newfollow = await follows.create({
-          followingid,
-          userid: req.user.id,
-          followerid: req.user.id,
-        });
-        await notis.create({
-          userId: req.user.id,
-          type: "FOLLOW",
-          text: "started following you.",
-          targetuserId: followingid,
-
-          followid: newfollow.id,
-        });
-        const newFollowing = await follows.findOne({
-          where: {
-            id: newfollow.id,
-          },
-          include: [
-            {
-              model: users,
-              as: "follower",
-              attributes: ["username", "avatar", "verified", "id"],
-            },
-            {
-              model: users,
-              as: "following",
-              attributes: ["username", "avatar", "verified", "id"],
-            },
-          ],
-        });
-        return res.status(200).send({ followed: true, newFollowing });
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send("Something went wrong");
+  try {
+    //get followingid from request body
+    const { followingid } = req.body;
+    //check if followingid is provided
+    if (!followingid) {
+      return res.status(400).send("followingid is required");
     }
+    //check if followingid is a number
+    else if (isNaN(followingid)) {
+      return res.status(400).send("Invalid followingid");
+    }
+    //check if user is trying to follow themselves
+    else if (followingid === req.user.id) {
+      return res.status(400).send("You cannot follow yourself");
+    } else {
+      //create async-lock key
+      const key = `follow-user-${followingid}-${req.user.id}`;
+      //acquire lock
+      await lock.acquire(
+        key,
+        async () => {
+          //check if the user exists and is not inactive
+          const finduser = await users.findOne({
+            where: {
+              id: followingid,
+            },
+          });
+          if (!finduser) {
+            return res
+              .status(400)
+              .send("You cannot follow a non-existent user");
+          } else if (finduser.status === "inactive") {
+            return res.status(400).send("You cannot follow an inactive user");
+          }
+          //check if follow relationship already exists
+          const [newFollow, created] = await follows.findOrCreate({
+            where: {
+              followingid,
+              userid: req.user.id,
+            },
+            defaults: {
+              followingid,
+              userid: req.user.id,
+              followerid: req.user.id,
+            },
+          });
+
+          //if follow relationship already exists, destroy it
+          if (!created) {
+            await follows.destroy({
+              where: {
+                followingid,
+                userid: req.user.id,
+              },
+            });
+            //return followed status as false
+            return res.status(200).send({ followed: false });
+          }
+          //if follow relationship does not exist, create it
+          else {
+            //create new notification
+            await notis.create({
+              userId: req.user.id,
+              type: "FOLLOW",
+              text: "started following you.",
+              targetuserId: followingid,
+              followid: newFollow.id,
+            });
+            //get details of new follow relationship
+            const newFollowing = await follows.findOne({
+              where: {
+                id: newFollow.id,
+              },
+              include: [
+                {
+                  model: users,
+                  as: "follower",
+                  attributes: ["username", "avatar", "verified", "id"],
+                },
+                {
+                  model: users,
+                  as: "following",
+                  attributes: ["username", "avatar", "verified", "id"],
+                },
+              ],
+            });
+            //return followed status as true and new follow relationship details
+            return res.status(200).send({ followed: true, newFollowing });
+          }
+        },
+        //set timeout to release lock after 5 seconds if not released by the code
+        { timeout: 5000 }
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    //error handling
+    return res.status(500).send("Something went wrong");
   }
 });
 
+//export router
 module.exports = router;
--- routes/POST/newpost.js
+++ routes/POST/newpost.js
@@ -11,86 +11,6 @@ const upload = require("../../utils/multermediaupload");
 const { sendmessage } = require("../../utils/discordbot");
 const { getLinkPreview } = require("link-preview-js");
 
-const uploadmedia = async (res, media) => {
-  try {
-    const imgsizelimit = 9437184; //9mb
-    const videosizelimit = 52428800; //50 mb
-
-    let mediaurl;
-    let mediakey;
-    let error;
-
-    //if media is not an image or video, send error
-
-    if (
-      !media?.mimetype.startsWith("image") &&
-      !media?.mimetype.startsWith("video")
-    ) {
-      error = "Media type not supported";
-    }
-
-    //if media size is not within limits send error
-    else if (
-      media?.size >
-      (media?.mimetype.startsWith("image") ? imgsizelimit : videosizelimit)
-    ) {
-      fs.unlink(media?.path, (err) => {
-        if (err) throw err;
-        console.log(`media deleted from server because of size limit`);
-      });
-
-      error = `${
-        media?.mimetype.startsWith("image") ? "image" : "video"
-      } size cannot exceed  ${
-        media?.mimetype.startsWith("image") ? "9mb" : "50mb"
-      }`;
-    } else {
-      //upload media to cloudinary
-
-      await cloudinary.uploader.upload(
-        media?.path,
-        {
-          upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
-          resource_type: "auto",
-        },
-        (err, result) => {
-          // if error, send error status
-          if (err) {
-            fs.unlink(media?.path, (err) => {
-              if (err) throw err;
-              console.log(
-                `media deleted from server after error uploading to cloudinary`
-              );
-            });
-            console.log(err);
-            error = "Error uploading media";
-          }
-          // if no error, delete media from server
-          fs.unlink(media?.path, (err) => {
-            if (err) throw err;
-            console.log(`media deleted from server after successful upload`);
-          });
-
-          mediaurl = result?.secure_url;
-          mediakey = result?.public_id;
-          if (result?.resource_type === "video") {
-            if (result?.width < result?.height) {
-              mediaurl = result?.secure_url.replace(
-                "upload/",
-                "upload/ar_1:1,b_black,c_pad/"
-              );
-            }
-          }
-        }
-      );
-    }
-
-    return [mediaurl, mediakey, error];
-  } catch (error) {
-    console.log("error");
-  }
-};
-
 router.post("/", upload.single("media"), async (req, res) => {
   try {
     const media = req.file ? req.file : null;
@@ -249,8 +169,88 @@ router.post("/", upload.single("media"), async (req, res) => {
   }
 });
 
-module.exports = router;
+// upload media to cloudinary and return url and key
+const uploadmedia = async (res, media) => {
+  try {
+    const imgsizelimit = 9437184; //9mb
+    const videosizelimit = 52428800; //50 mb
 
+    let mediaurl;
+    let mediakey;
+    let error;
+
+    //if media is not an image or video, send error
+
+    if (
+      !media?.mimetype.startsWith("image") &&
+      !media?.mimetype.startsWith("video")
+    ) {
+      error = "Media type not supported";
+    }
+
+    //if media size is not within limits send error
+    else if (
+      media?.size >
+      (media?.mimetype.startsWith("image") ? imgsizelimit : videosizelimit)
+    ) {
+      fs.unlink(media?.path, (err) => {
+        if (err) throw err;
+        console.log(`media deleted from server because of size limit`);
+      });
+
+      error = `${
+        media?.mimetype.startsWith("image") ? "image" : "video"
+      } size cannot exceed  ${
+        media?.mimetype.startsWith("image") ? "9mb" : "50mb"
+      }`;
+    } else {
+      //upload media to cloudinary
+
+      await cloudinary.uploader.upload(
+        media?.path,
+        {
+          upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET,
+          resource_type: "auto",
+        },
+        (err, result) => {
+          // if error, send error status
+          if (err) {
+            fs.unlink(media?.path, (err) => {
+              if (err) throw err;
+              console.log(
+                `media deleted from server after error uploading to cloudinary`
+              );
+            });
+            console.log(err);
+            error = "Error uploading media";
+          }
+          // if no error, delete media from server
+          fs.unlink(media?.path, (err) => {
+            if (err) throw err;
+            console.log(`media deleted from server after successful upload`);
+          });
+
+          mediaurl = result?.secure_url;
+          mediakey = result?.public_id;
+          if (result?.resource_type === "video") {
+            if (result?.width < result?.height) {
+              mediaurl = result?.secure_url.replace(
+                "upload/",
+                "upload/ar_1:1,b_black,c_pad/"
+              );
+            }
+          }
+        }
+      );
+    }
+
+    return [mediaurl, mediakey, error];
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+// add preview link to database
 const addpreviewlink = async (newPost) => {
   const findurlregex = /(https?:\/\/)(?!localhost|127\.0\.0\.1)[^\s]+\.[a-z]+/;
   const link = findurlregex.exec(newPost?.text);
@@ -293,15 +293,4 @@ const addpreviewlink = async (newPost) => {
   }
 };
 
-// {
-//   url: 'https://www.google.com/',
-//   title: 'Google',
-//   siteName: undefined,
-//   description: "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking for.",
-//   mediaType: 'website',
-//   contentType: 'text/html',
-//   images: [
-//     'https://www.google.com/images/branding/googlelogo/1x/googlelogo_white_background_color_272x92dp.png'
-//   ],
-//   videos: [],
-//   favicons: [ 'https://www.google.com/favicon.ico' ]
+module.exports = router;
