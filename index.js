1a2982ed4ed827362fe34cf93fe56b2e15bd0edb
Nawang17 <nawangofficial1010@gmail.com>
2023-06-05 00:57:38 -0400

    allow multiple origins

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const blacklistMiddleware = (req, res, next) => {
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: ["http://localhost:3000", "https://momosz.com"],
   })
 );
 app.set("trust proxy", 4);
