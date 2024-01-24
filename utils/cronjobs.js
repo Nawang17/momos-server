ea55de51e99647975ecedcae23f89e540bedde4c
Nawang <77951020+Nawang17@users.noreply.github.com>
2024-01-24 13:36:31 -0500

    improved addPostTranslations cronjob

--- utils/cronjobs.js
+++ utils/cronjobs.js
@@ -2,10 +2,11 @@ const cron = require("node-cron");
 const { posts, translations } = require("../models");
 const { translate } = require("@vitalets/google-translate-api");
 const { Op } = require("sequelize");
-// this cron job will run once every hour
+
+// This cron job will run once every hour
 const addPostTranslations = cron.schedule("0 * * * *", async () => {
   try {
-    //get all posts
+    // Get all posts
     const postData = await posts.findAll({
       where: {
         language: null,
@@ -13,50 +14,76 @@ const addPostTranslations = cron.schedule("0 * * * *", async () => {
           [Op.ne]: null,
         },
       },
-      order: [["id", "DESC"]], //order by id desc to get the latest posts first
+      order: [["id", "DESC"]],
     });
+
     const converted = JSON.parse(JSON.stringify(postData));
-    //for each posts get the translations and update the post language wiht the src language and if src lang is en then make another translation to ko and add that to trnaslations table
+
+    // For each post, get the translations and update the post language with the source language
     for (let i = 0; i < converted?.length; i++) {
       const currentpost = converted[i];
+
       if (!currentpost) {
         continue;
       }
-      await translate(currentpost?.text).then(async (res) => {
-        await posts.update(
-          { language: res?.raw?.src },
-          { where: { id: currentpost?.id } }
-        );
-        if (res?.raw?.src !== "ko") {
-          await translate(
-            currentpost?.text,
 
-            {
-              to: "ko",
-            }
-          ).then(async (res) => {
+      try {
+        const res = await translate(currentpost?.text);
+
+        if (res && res.raw && res.raw.src && res.text) {
+          // Update post language
+          await posts.update(
+            { language: res.raw.src },
+            { where: { id: currentpost.id } }
+          );
+             // Add translation to translations table if source language is not "en"
+          if (res.raw.src !== "en") {
             await translations.create({
-              translatedText: res?.text,
-              language: "ko",
-              postId: currentpost?.id,
+              translatedText: res.text,
+              language: "en",
+              postId: currentpost.id,
             });
-          });
+          }
+
+          // Add translation to translations table if source language is not "ko"
+          if (res.raw.src !== "ko") {
+            try {
+              const koTranslation = await translate(currentpost.text, {
+                to: "ko",
+              });
+
+              await translations.create({
+                translatedText: koTranslation.text,
+                language: "ko",
+                postId: currentpost.id,
+              });
+            } catch (koTranslationError) {
+              console.error('Korean translation error:', koTranslationError);
+              // Remove language if there was an error during Korean translation
+              await posts.update(
+                { language: null },
+                { where: { id: currentpost.id } }
+              );
+
+              // delete any translatiosn for the post if there was an error during Korean translation
+              await translations.destroy({
+                where: { postId: currentpost.id },
+              });
+    
+            }
+          }
+         
+        } else {
+          console.error('Translation response is missing expected properties:', res);
         }
-        if (res?.raw?.src !== "en") {
-          await translations.create({
-            translatedText: res?.text,
-            language: "en",
-            postId: currentpost?.id,
-          });
-        }
-      });
+      } catch (error) {
+        console.error('Translation error:', error);
+      }
     }
 
     console.log("addPostTranslations executed");
-    return;
   } catch (error) {
-    console.log("addPostTranslations executed error", error);
-    return;
+    console.error("addPostTranslations executed error", error);
   }
 });
 
